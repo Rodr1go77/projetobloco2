b61da5df30f14ba9f93fdeb84bd02bd5e0ed1ec2
Rodr1go77 <rodrigo.bisol@gmail.com>
2025-09-29 18:41:37 -0300

    TelaAuth

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,45 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Typography, Divider, autocompleteClasses } from "@mui/material";
 
 export default function Main() {
   return (
-    <div>
-      MAIN  - BET RIGHT  NOW NOVO COMMIT
-    </div>
+    <>
+      <Box
+        sx={{
+          marginBottom: 5,
+          display: "flex",
+          flexDirection: "column",
+          width: 450,
+          alignItems: "center",
+          border: "2px solid white",
+          borderRadius: "7px",
+          padding: 4,
+          margin: "auto",
+        }}
+      >
+        <Typography
+          sx={{ fontWeight: "bolder", fontSize: "2rem", marginBottom: 3, color: "white" }}
+        >
+          {" "}
+          BET RIGHT
+        </Typography>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nome"
+          defaultValue="Digite o seu nome:"
+          sx={{ width: 400 }}
+        />
+        <Divider> Divisor </Divider>
+        <TextField
+          required
+          id="outlined-required"
+          label="E-mail"
+          defaultValue="Digite o seu e-mail:"
+          sx={{ width: 400 }}
+        />
+      </Box>
+    </>
   );
 }
