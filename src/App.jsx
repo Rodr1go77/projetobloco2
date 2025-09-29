262db3b4cabb1fa251c7b5b5ef1aa5e0944153d8
Rodr1go77 <rodrigo.bisol@gmail.com>
2025-09-29 20:31:50 -0300

    Added 3 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css"
 import Main from "./Components/Main";
+import InfoPartida from "./Components/InfoPartida";
+import JogoResponsavel from "./Components/JogoResponsavel"
+import AnalisePartida from "./Components/AnalisePartida"
 
 export default function App() {
   return (
     <div >
       <Main/>
+      <InfoPartida/>
+      <JogoResponsavel/>
+      <AnalisePartida/>
     </div>
   );
 }
