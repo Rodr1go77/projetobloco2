1690a65381990542bce20c62d51685b26a0ff044
Rodr1go77 <rodrigo.bisol@gmail.com>
2025-10-14 17:51:46 -0300

    Pages dentro de Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css"
-import Login from "./Components/Login";
-import Registro from "./Components/Registro"
-import EsqueciMinhaSenha from "./Components/EsqueciMinhaSenha"
-import Home from "./Components/Home";
-import AnalisePartida from "./Components/AnalisePartida"
-import Perfil from "./Components/Perfil"
+import Login from "./Pages/Login";
+import Registro from "./Pages/Registro"
+import EsqueciMinhaSenha from "./Pages/EsqueciMinhaSenha"
+import Home from "./Pages/Home";
+import AnalisePartida from "./Pages/AnalisePartida"
+import Perfil from "./Pages/Perfil"
 
 export default function App() {
   return (
