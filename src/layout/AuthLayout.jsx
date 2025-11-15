ca52467a04c74fbe4ab2d7516a5eb0b8b59da45d
Rodr1go77 <rodrigo.bisol@gmail.com>
2025-11-15 11:00:21 -0300

    Ajuste para modo Producao

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom";
 import NavMenu from "../Pages/NavMenu"
 import RoutesPath from "../routes/RoutesPath";
+import { FlutterDashOutlined } from "@mui/icons-material";
 
 export default function AuthLayout({isAuthenticated}) {
 
-  const dev_preview = true
+  const dev_preview = FlutterDashOutlined
 
 
   if (!isAuthenticated && !dev_preview) {
